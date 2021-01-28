24da1053c9cd0428ae3f1737957ae93a40e9bd3f
hsy <no02031@naver.com>
2021-01-28 16:27:22 +0900

    Add Access Modifier to Bank

--- Bank/src/BankAccount.java
+++ Bank/src/BankAccount.java
@@ -1,22 +1,38 @@
 public class BankAccount {
-    int balance;
-    Person owner;
+    private int balance;
+    private Person owner;
+
+    public void setBalance(int newBalance) {
+        balance = newBalance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setOwner(Person newPerson) {
+        owner = newPerson;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
 
     // 파라미터 : 입금할 액수 (정수)
     // 리턴 : 성공여부 (불린)
     boolean deposit(int amount) {
         if (amount > 0) {
-            if (owner.cashAmount < amount) {
-                System.out.println("입금 실패입니다. 잔고: " + balance + "원, 현금: " + owner.cashAmount + "원");
+            if (owner.getCashAmount() < amount) {
+                System.out.println("입금 실패입니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
                 return false;
             } else {
                 balance += amount;
-                owner.cashAmount -= amount;
-                System.out.println(amount + "원 입금하였습니다. 잔고: " + balance + "원, 현금: " + owner.cashAmount + "원");
+                owner.setCashAmount(owner.getCashAmount()-amount);
+                System.out.println(amount + "원 입금하였습니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
                 return true;
             }
         }
-        System.out.println("입금 실패입니다. 잔고: " + balance + "원, 현금: " + owner.cashAmount + "원");
+        System.out.println("입금 실패입니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
         return false;
     }
 
@@ -25,16 +41,16 @@ public class BankAccount {
     boolean withdraw(int amount) {
         if (amount > 0) {
             if (balance < amount) {
-                System.out.println("출금 실패입니다. 잔고: " + balance + "원, 현금: " + owner.cashAmount + "원");
+                System.out.println("출금 실패입니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
                 return false;
             } else {
                 balance -= amount;
-                owner.cashAmount += amount;
-                System.out.println(amount + "원 출금하였습니다. 잔고: " + balance + "원, 현금: " + owner.cashAmount + "원");
+                owner.setCashAmount(owner.getCashAmount()+amount);
+                System.out.println(amount + "원 출금하였습니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
                 return true;
             }
         }
-        System.out.println("출금 실패입니다. 잔고: " + balance + "원, 현금: " + owner.cashAmount + "원");
+        System.out.println("출금 실패입니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
         return false;
     }
 
